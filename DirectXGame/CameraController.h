3c91c5a1f76228e63100472ae703875f689473aa
Noir-0409 <146914778+Noir-0409@users.noreply.github.com>
2025-09-05 11:48:21 +0900

    座標ずらした

--- DirectXGame/CameraController.h
+++ DirectXGame/CameraController.h
@@ -26,10 +26,11 @@ Camera* camera_ = nullptr;
 
 Player* target_ = nullptr;
 
-Vector3 targetOffset_ = {0, 0, -30.0f};
+Vector3 targetOffset_ = {0, 10.0f, -30.0f};
 
 Vector3 targetPosition_;
 
-static inline const float kInterpolationRate_ = 0.1f;
+static inline const float kInterpolationRate_ = 0.05f;
+
 
 };
