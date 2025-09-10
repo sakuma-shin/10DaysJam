9e57f38c25637f6387b3f39df4768f0c20f850be
osinoko <k023g0114@g.neec.ac.jp>
2025-09-10 16:20:39 +0900

    死亡判定追加

--- DirectXGame/Player.h
+++ DirectXGame/Player.h
@@ -14,7 +14,13 @@ public:
 
 	void Draw(Camera& camera);
 
+	void Collision();
+
 	const WorldTransform& GetWorldTransform() const { return worldTransform_; }
+
+	Vector3& Gettranslate() { return worldTransform_.translation_; }
+
+	bool isDead() const { return isdead_; } 
 
 private:
 	WorldTransform worldTransform_;
@@ -29,5 +35,7 @@ private:
 	const float gravity_ = -0.10f;
 	const float jumpPower_ = 2.0f;
 	const float groundHeight_ = 0.0f;
+
+	bool isdead_ = false;
 
 };
